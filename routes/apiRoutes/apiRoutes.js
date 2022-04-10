623feaa190b130047163b917de70b7e90c436efd
Carl Bush <CarlBushs13@gmail.com>
2022-04-10 12:53:53 -0700

    apiRoutes get Method

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -2,8 +2,20 @@ const path = require("path");
 const router = require("express").Router();
 const db = require("../../db/db.json");
 
+//RETRIEVES ALL DATA IN THE JSON FILE
 router.get("/", (req, res) => {
     res.json(db);
+});
+
+//RETRIVES ONLY THE SELECTED ID
+router.get("/:id", (req, res) => {
+    const found = db.some(data => data.id == req.params.id);
+
+    if(found){
+        res.json(db.filter(data=> data.id == req.params.id));
+    } else {
+        res.status(400).json({ Message: `No notes with the id of ${req.params.id} found`})
+    };
 });
 
 
